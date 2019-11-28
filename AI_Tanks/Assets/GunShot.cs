ddd930dc434e566f1078a23adf74f3bddb1bf891
Morrris87 <zach.morris1999@gmail.com>
2019-11-28 16:44:14 -0500

    New 4:44PM

---
+++ AI_Tanks/Assets/GunShot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GunShot : MonoBehaviour
+{
+    public float damage = 10.0f;
+    public float range = 100f;
+    public float impactForce = 10000000;
+
+    public Camera fpsCam;
+    public ParticleSystem MuzzelFlash;
+    public GameObject ImpactEffect;
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        MuzzelFlash.Play();
+
+        RaycastHit hit;
+       if( Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            TargetHit target = hit.transform.GetComponent<TargetHit>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * 300);
+            }
+
+            GameObject ImpactGO = Instantiate(ImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(ImpactGO, 2f);
+        }
+    }
+}
---
+++ AI_Tanks/Assets/TargetHit.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class TargetHit : MonoBehaviour
+{
+
+    public float health = 50.0f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
