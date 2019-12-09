60c29761bba486e7d5aab7c6d5489d3dd5e57ebf
Morrris87 <58307420+Morrris87@users.noreply.github.com>
2019-12-09 13:02:39 -0500

    new

---
+++ AI_Tanks/Assets/Scripts/AiStates/AI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+
+public class AI : MonoBehaviour
+{
+    public bool SwitchState = false;
+    public bool SeenPlayer = false;
+    public bool LowHealth = false;
+    public bool InRange = false;
+    public bool NoHealth = false;
+
+    float range = 10;
+    float health = 50;
+    public FSM<AI> stateMachine { get; set; }
+
+    private void Start()
+    {
+        stateMachine = new FSM<AI>(this);
+        stateMachine.ChangeState(WanderState.Instance);
+    }
+    private void Update()
+    {
+        //stuff here for switching between states
+        stateMachine.Update();
+    }
+}
---
+++ AI_Tanks/Assets/Scripts/AiStates/FleeState.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+
+public class FleeState : State<AI>
+{
+    private static FleeState _instance;
+
+    private FleeState()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+    public static FleeState Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new FleeState();
+            }
+            return _instance;
+        }
+    }
+
+    public override void EnterState(AI _owner)
+    {
+        Debug.Log("Entering Flee State");
+    }
+
+    public override void ExitState(AI _owner)
+    {
+        Debug.Log("Exiting Flee State");
+    }
+
+    public override void UpdateState(AI _owner)
+    {
+        if(!_owner.LowHealth)
+        {
+            _owner.stateMachine.ChangeState(WanderState.Instance);
+        }
+        else if (_owner.NoHealth)
+        {
+            _owner.stateMachine.ChangeState(DeathState.Instance);
+        }
+
+    }
+}
