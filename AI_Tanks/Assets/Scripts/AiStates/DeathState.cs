3508dffd5e29e38c7187f2de866c71c384a5fa3c
zaclink97 <51802916+zaclink97@users.noreply.github.com>
2019-12-09 12:40:56 -0500

    POOP SHIT

---
+++ AI_Tanks/Assets/Scripts/AiStates/DeathState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+
+public class DeathState : State<AI>
+{
+    private static DeathState _instance;
+
+    private DeathState()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+    public static DeathState Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new DeathState();
+            }
+            return _instance;
+        }
+    }
+
+    public override void EnterState(AI _owner)
+    {
+        Debug.Log("Entering Death State");
+    }
+
+    public override void ExitState(AI _owner)
+    {
+        Debug.Log("Exiting Daeth State");
+    }
+
+    public override void UpdateState(AI _owner)
+    {
+
+    }
+}
